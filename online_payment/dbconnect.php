2b9884bcf106d7b11248ef39582166fc3ee407ec
nicogarce <ngarce36@gmail.com>
2025-10-18 09:45:27 +0800

    update

--- online_payment/dbconnect.php
+++ online_payment/dbconnect.php
@@ -1,5 +1,5 @@
 <?php    
-$con = mysqli_connect("localhost","uphsledu_dragpay","@dragonpay#","uphsledu_onlinepayment");    
+$con = mysqli_connect("localhost","root","","uphsledu_onlinepayment");    
 if(!$con){  	echo "FAILED TO CONNECT";  }  else {  	echo "";  }  
 ?>
 
